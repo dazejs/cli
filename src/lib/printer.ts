10c68d9f7077665ad85fc8801ce9ba04836477c4
Zewail <chanzewail@gmail.com>
2020-06-01 11:48:20 +0800

    Adapter 3.0 (#2)
    * chore(ts): update
    
    * chore(add): add files
    
    * feat(re): ts

---
+++ src/lib/printer.ts
@@ -0,0 +1,18 @@
+
+
+
+
+export class Printer {
+  static power() {
+    console.log('');
+    console.log('');
+    console.log('        ____  ___ _____   ______    _______');
+    console.log('       / __ \\/   /__  /  / ____/   / / ___/');
+    console.log('      / / / / /| | / /  / __/ __  / /\\__ \\ ');
+    console.log('     / /_/ / ___ |/ /__/ /___/ /_/ /___/ / ');
+    console.log('    /_____/_/  |_/____/_____/\\____//____/  ');
+    console.log('');
+    console.log('');
+    console.log('Visit https://dazejs.org/ to learn more.');
+  }
+}
